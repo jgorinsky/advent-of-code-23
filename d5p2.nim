9c1f4713bb2cbe2b4f3a45de6d9ff606a7741665
Justin Gorinsky <gorinsky@gmail.com>
2023-12-10 00:41:58 -0600

    improved memory efficiency

--- d5p2.nim
+++ d5p2.nim
@@ -4,8 +4,6 @@ import std/sugar
 import std/tables
 import std/enumerate
 
-
-
 let file = "d5.txt"
 
 type Mapping = object
@@ -16,6 +14,7 @@ type Mapping = object
 type Mappings = seq[Mapping]
 proc newMap(): Mappings = newSeq[Mapping]()
 
+var seedDef: string
 var maps = {
     "seed": newMap(),
     "soil": newMap(),
@@ -25,34 +24,11 @@ var maps = {
     "temperature": newMap(),
     "humidity": newMap(),
 }.toOrderedTable
-var seeds: seq[int]
-
-proc findMapping(mappings: Mappings, val: int): int =
-    for mapping in mappings:
-        if val >= mapping.srcStart and val <= mapping.srcEnd:
-            return mapping.destStart + (val - mapping.srcStart)
-    return val
-
-proc traverse(seed: int): int = 
-    var r = seed    
-    for key in maps.keys:
-        r = findMapping(maps[key], r)
-    return r
 
 var currentMap: string
 for i, line in enumerate(file.lines):
     if i == 0:
-        let seedRanges = collect:
-            let vals = line.split(":")[1]
-                .split(" ")
-                .filterIt(it.isEmptyOrWhitespace.not)
-                .mapIt(it.parseInt)
-            for i, x in vals:
-                if i mod 2 == 0:
-                    (x, vals[i+1])
-
-        for rng in seedRanges:
-            seeds &= toSeq(rng[0]..rng[0] + rng[1])
+        seedDef = line
         continue
 
     if line.isEmptyOrWhitespace:
@@ -75,4 +51,37 @@ for i, line in enumerate(file.lines):
         srcEnd: parts[1] + parts[2] - 1,
     ))
 
-echo seeds.map(traverse).min
+iterator seedIter(line: string): int =
+    let seedRanges = collect:
+        let vals = line.split(":")[1]
+            .split(" ")
+            .filterIt(it.isEmptyOrWhitespace.not)
+            .mapIt(it.parseInt)
+        for i, x in vals:
+            if i mod 2 == 0:
+                (x, vals[i+1])
+
+    for rng in seedRanges:
+        for i in countup(rng[0], rng[0] + rng[1] - 1):
+            yield i
+            
+proc findMapping(mappings: Mappings, val: int): int =
+    for mapping in mappings:
+        if val >= mapping.srcStart and val <= mapping.srcEnd:
+            return mapping.destStart + (val - mapping.srcStart)
+    return val
+
+
+proc traverse(seed: int): int = 
+    var r = seed
+    for key in maps.keys:
+        r = findMapping(maps[key], r)
+    return r
+
+
+var min = 99999999999999999
+for seed in seedIter(seedDef):
+    let r = traverse(seed)
+    if r < min:
+        min = r
+echo min
